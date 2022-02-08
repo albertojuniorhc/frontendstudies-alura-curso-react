2b1aa9db0589df136b85aa4b12fc369e8737a6c0
Alberto Jr. (Beto) <albertojuniorhc@gmail.com>
2022-02-08 11:18:11 -0300

    Aula3 finalizada (#1)

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,34 +2,46 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Diversos";
   }
 
-  _handleMudancaTitulo(evento){
-    evento.stopPropagation(); 
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handleMudancaTexto(evento){
+  _handleMudancaTexto(evento) {
     evento.stopPropagation();
     this.texto = evento.target.value;
   }
 
-  _criarNota(evento){
+  _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto)
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
+  }
+
+  _handeleMudancaCategoria(evento){
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
   }
 
   render() {
     return (
-      <form className="form-cadastro"
-        onSubmit={this._criarNota.bind(this)}
-      >
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handeleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option>Diversos</option>
+          {this.props.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>;
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
